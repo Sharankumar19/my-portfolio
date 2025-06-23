01d0d3fdde2001165e4b635a3043e7d12697c62c
icnk0048-sharankumar <sharankarunakaran19@gmail.com>
2025-06-23 23:09:51 +0530

    comment code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ function Header(props){
         <div className='nav-container'>
             <p className="logo" href="#">{props.logo}</p>
             <ul className='nav-menu'>
-                <li><AnchorLink className='anchor-link' offset={50} href='#hero'><p>Home</p></AnchorLink></li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#about'><p>About Me</p></AnchorLink></li>
+                {/* <li><AnchorLink className='anchor-link' offset={50} href='#hero'><p>Home</p></AnchorLink></li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#about'><p>About Me</p></AnchorLink></li> */}
                 <li><AnchorLink className='anchor-link' offset={50} href='#skills'><p>Skills</p></AnchorLink></li>
                 {/* <li><AnchorLink className='anchor-link' offset={50}><p>Project</p></AnchorLink></li> */}
                 <li><AnchorLink className='anchor-link' offset={50} href='#contact' ><p>Contact</p></AnchorLink></li>
